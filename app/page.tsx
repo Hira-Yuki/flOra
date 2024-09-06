da18a6a65af87d2eff3e0109791c83c3465b3c8a
Hira-Yuki <cjsdlf44@gmail.com>
2024-09-06 22:38:26 +0900

    style : 반응형 텍스트 적용

--- app/page.tsx
+++ app/page.tsx
@@ -100,7 +100,7 @@ export default function Hello() {
           </form>
           <div className="flex items-center w-full mt-8">
             <hr className="border-gray-300 w-1/5" />
-            <span className="mx-4 text-xs text-gray-500 w-3/5 text-center text-nowrap">
+            <span className="mx-4 text-xs sm:text-sm md:text-xs text-gray-500 w-3/5 text-center text-nowrap">
               SNS 계정으로 시작하기
             </span>
             <hr className="border-gray-300 w-1/5" />
