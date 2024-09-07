fbcc16ad886898330b56f521f3142bdf4fbeed93
Hira-Yuki <cjsdlf44@gmail.com>
2024-09-08 01:18:13 +0900

    feat : BASE_URL 배포환경에서 사용할 수 있도록 수정

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:3000' as const;
+export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const createAxiosInstance = (baseURL: string = BASE_URL) => {
   return axios.create({
