66a25d800b1e726b1678ccc1640cfe9de18638e5
Hira-Yuki <cjsdlf44@gmail.com>
2024-09-07 22:00:08 +0900

    feat : mock api 세팅

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = {
       '@components': path.resolve(__dirname, 'components'),
       '@hooks': path.resolve(__dirname, 'hooks'),
       '@styles': path.resolve(__dirname, 'styles'),
+      '@api': path.resolve(__dirname, 'api'),
     };
     return config;
   },
