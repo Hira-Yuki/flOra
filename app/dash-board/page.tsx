22f5d1f7e50959d846063a448e3ba853f0d550dd
Hira-Yuki <cjsdlf44@gmail.com>
2024-09-08 13:23:56 +0900

    feat : 대시보드 레이아웃 마크업

--- app/dash-board/page.tsx
+++ app/dash-board/page.tsx
@@ -1,28 +1,34 @@
 export default function DashBoard() {
   return (
-    <div className="h-screen w-screen relative">
-      <div className="h-screen w-24 bg-green-500 absolute flex flex-col items-center pt-4">
-        <ul className="flex flex-col gap-10 h-full">
-          <li className="w-14 h-14 bg-gray-600 text-center flex items-center justify-center">
+    <div className="h-screen w-screen flex flex-col">
+      {/* 사이드바 */}
+      <div className="h-full w-24 bg-gray-300 absolute flex flex-col items-center pt-6">
+        <ul className="flex flex-col gap-10 h-full text-white">
+          <li className="w-14 h-14 bg-gray-500 text-center flex items-center justify-center">
             홈
           </li>
-          <li className="w-14 h-14 bg-gray-600 text-center flex items-center justify-center">
+          <li className="w-14 h-14 bg-gray-500 text-center flex items-center justify-center">
             캘린더
           </li>
-          <li className="w-14 h-14 bg-gray-600 text-center flex items-center justify-center">
+          <li className="w-14 h-14 bg-gray-500 text-center flex items-center justify-center">
             타이머
           </li>
-          <li className="w-14 h-14 bg-gray-600 text-center flex items-center justify-center">
+          <li className="w-14 h-14 bg-gray-500 text-center flex items-center justify-center">
             플로라
           </li>
-          <li className="w-14 h-14 bg-gray-600 text-center flex items-center justify-center mt-auto mb-10">
+          <li className="w-14 h-14 bg-gray-500 text-center flex items-center justify-center mt-auto mb-6">
             프로필
           </li>
         </ul>
       </div>
-      <div className="h-24 w-full pl-24 bg-red-500">
-        <h2>header</h2>
+
+      {/* 헤더 */}
+      <div className="h-24 w-full pl-24 py-8 flex items-center">
+        <h2 className="ml-8 text-4xl">Dashboard</h2>
       </div>
+
+      {/* 메인 콘텐츠 */}
+      <div className="flex-1 ml-24">내용 : 몬가... 한다...</div>
     </div>
   );
 }
