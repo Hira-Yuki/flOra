cd09d4bbc6dbcac0b0072cc1a91e326b4e8256a7
Hira-Yuki <cjsdlf44@gmail.com>
2024-09-29 00:01:29 +0900

    feat : Suspense 추가
    - 동적할당을 위해 Suspense 추가

--- app/(sign)/reset-password/page.tsx
+++ app/(sign)/reset-password/page.tsx
@@ -1,11 +1,15 @@
+import Loading from '@components/Loading';
 import ResetPasswordForm from '@components/ResetPasswordForm';
 import { SignInTitle } from '@components/signInElements';
+import { Suspense } from 'react';
 
 export default function ResetPassword() {
   return (
     <>
       <SignInTitle text={'비밀번호 재설정'} />
-      <ResetPasswordForm />
+      <Suspense fallback={<Loading />}>
+        <ResetPasswordForm />
+      </Suspense>
     </>
   );
 }
