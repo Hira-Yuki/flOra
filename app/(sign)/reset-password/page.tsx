6a0398972afc1d02dc51e407c4bc41646273d672
Hira-Yuki <cjsdlf44@gmail.com>
2024-09-28 18:14:58 +0900

    fix : 오타 수정

--- app/(sign)/reset-password/page.tsx
+++ app/(sign)/reset-password/page.tsx
@@ -1,7 +1,5 @@
 import ResetPasswordForm from '@components/ResetPasswordForm';
 
 export default function ResetPassword() {
-  return;
-
-  <ResetPasswordForm />;
+  return <ResetPasswordForm />;
 }
